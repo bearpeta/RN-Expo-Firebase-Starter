a4c7534d5993615338a59b56f2dd1e0588b18c2c
Alessandro Petrillo <aless.petrillo@gmail.com>
2019-04-18 16:25:06 -0400

    initial commit

---
+++ README.md
@@ -0,0 +1,10 @@
+# RN-Expo-Firebase-Starter
+This project can be used as a boilerplate when you want to use Firebase in the RN Expo environment.
+
+THIS PROJECT IS NOT READY TO USE YET!!! Some final tests have to be done before and the installation guide/README has to be finished. 
+
+Installation steps:
+
+1) Be sure you have the expo-client, node and npm installed.
+2) Add your google-services.json to the android/app folder.
+3) Add your GoogleService-Info.plist to the ios folder.
---
+++ ios/rnexpofirebasestarter/AppDelegate.m
@@ -0,0 +1,16 @@
+// Copyright 2015-present 650 Industries. All rights reserved.
+#import <Firebase.h>
+#import "AppDelegate.h"
+
+@implementation AppDelegate
+
+// Put your app delegate methods here. Remember to also call methods from EXStandaloneAppDelegate superclass
+// in order to keep Expo working. See example below.
+
+- (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
+{
+  [FIRApp configure];
+  return [super application:application didFinishLaunchingWithOptions:launchOptions];
+}
+
+@end
